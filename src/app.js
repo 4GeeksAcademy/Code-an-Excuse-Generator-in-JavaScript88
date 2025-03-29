bf4f8ad1330ebfde7ea6890b7db7f47b5c85e13d
Abiri Essockamba <stevynaulax88@gmail.com>
2025-03-29 18:21:15 +0000

    fin del proyecto generador de excusa

--- src/app.js
+++ src/app.js
@@ -4,8 +4,34 @@ import "./style.css";
 
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
+        //.     0           1             2            3          4
+let who = ['The dog', 'My grandma', 'The mailman', 'My bird', 'Biggie'];
+let action = ['ate', 'peed', 'crushed', 'broke'];
+let what = ['my homework', 'my phone', 'the car'];
+let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch !', 'while I was praying'];
 
-window.onload = function() {
-  //write your code here
+
+function getRandomNumber(a, b)
+{
+return Math.floor(Math.random() * (b - a) + a);
+}
+
+ function getRandom(anyArray)
+{
+let max = anyArray.length -1;
+let min = 0;
+console.log(max, min, anyArray)
+let random = getRandomNumber(min, max);
+return anyArray[random];
+}
+
+
+
+window.onload = function()
+
+{
+
+  document.querySelector("#excuse").innerHTML = getRandom(who) +" "+ getRandom(action) +" "+ getRandom(what) +" "+ getRandom(when);
+
   console.log("Hello Rigo from the console!");
 };
